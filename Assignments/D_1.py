a0eff2d826472e2fe6e8db727f46b120e15a8dee
Sravanthi <sravanthivaddem@gmail.com>
2025-09-02 17:48:33 +0530

    first commit

--- Assignments/D_1.py
+++ Assignments/D_1.py
@@ -1,14 +1,1 @@
-from PIL import Image
-from IPython.display import display
-
-img = Image.open(r"C:\\Users\\Sravanthi\\Pictures\\Screenshots\\Screenshot 2025-09-01 164652.png")
-print("this is Disk Management after shrinking the volume and created the new partition D DRIVE")
-display(img)
-
-img1 = Image.open(r"C:\\Users\\Sravanthi\\Pictures\\Screenshots\\Screenshot 2025-09-01 164937.png")
-print("This is the final folder structure of Course work in D DRIVE")
-display(img1)
-
-img2 = Image.open(r"C:\\Users\\Sravanthi\\Pictures\\Screenshots\\Screenshot 2025-09-01 165412.png")
-print("This is the final folder structure.")
-display(img2)
+print("hello world")
