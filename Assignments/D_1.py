c53e58c1376ec232108bc0ce749ae029568ebcf6
Sravanthi <sravanthivaddem@gmail.com>
2025-09-03 16:24:29 +0530

    final_3

--- Assignments/D_1.py
+++ Assignments/D_1.py
@@ -1,2 +1,8 @@
-a = 5
-print(a)
+def simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+principal=float(input("Enter the principal amount: "))
+rate=float(input("Enter the rate of interest: "))   
+time=float(input("Enter the duration in years: "))
+sim_int=simple_interest(principal, rate, time)
+print("The simple interest is:", sim_int)
